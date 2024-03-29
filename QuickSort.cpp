d1930a20e4e025d2f808c9cd5467d31600d71768
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-03-29 13:50:51 +0700

    Menambahkan Prosedur input

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -6,4 +6,23 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input(){
+	while (true) 
+    {
+		cout << " Masukan banyaknya elemen pada array : "; // menampilkan ke layar
+		cin >> n; // input dari user
+		if (n <= 20) // jika kurang atausama dengan 20
+			break; // keluar dari loop
+		else
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // menampilkan ke layar
+    }
+	cout << "\n=====================" << endl;
+	cout << "\nMasukan Elemen Array" << endl;
+	cout << "\n=====================" << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + i) << " > ";
+		cin >> arr[i];
+    }
+}
